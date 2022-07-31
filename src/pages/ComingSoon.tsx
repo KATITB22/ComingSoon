757f39bb955118c5a649b48f9155ce9d97166e2c
ravielze <ravielz20@gmail.com>
2022-07-31 13:20:28 +0700

    Edit font ligatures

--- src/pages/ComingSoon.tsx
+++ src/pages/ComingSoon.tsx
@@ -1,22 +1,25 @@
+const title = 'a b 2022';
+const comingSoon = 'omin oo';
+
 export const ComingSoon = () => (
     <>
         <div className="m-0 p-0 lg:bg-cover lg:bg-no-repeat lg:bg-center lg:bg-local lg:min-h-screen background lg:flex lg:flex-col lg:justify-center lg:text-center">
             <div className="hidden lg:block">
                 <h1 className="md:text-8xl lg:text-9xl bold lg:text-blue-900">
-                    KAT ITB 2022
+                    {title}
                 </h1>
                 <h1 className="md:text-6xl lg:text-7xl lg:text-blue-800">
-                    Coming Soon
+                    {comingSoon}
                 </h1>
             </div>
         </div>
         <div className="m-0 p-0 lg:hidden bg-cover bg-no-repeat bg-center bg-local min-h-screen background-portrait flex flex-col justify-center text-center">
             <div className="lg:hidden">
                 <h1 className="text-5xl md:text-8xl bold text-blue-900">
-                    KAT ITB 2022
+                    {title}
                 </h1>
                 <h1 className="text-3xl md:text-6xl text-blue-800">
-                    Coming Soon
+                    {comingSoon}
                 </h1>
             </div>
         </div>
